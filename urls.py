9677c1bf7b00f144edb320deb87d655dd90cfa9f
Jasurbek <154021388+JasurbekKarimov@users.noreply.github.com>
2024-03-08 12:33:44 +0500

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from authentication.views import RegisterView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from . import views
+
+
+
+
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('register/', RegisterView.as_view(), name='auth_register'),
+    path('', views.ListCreateMovieAPIView.as_view(), name='get_post_movies'),
+    path('<int:pk>/', views.RetrieveUpdateDestroyMovieAPIView.as_view(), name='get_delete_update_movie'),
 ]
--- views.py
+++ views.py
@@ -1,10 +1,32 @@
-from django.contrib.auth.models import User
-from rest_framework import generics
-from rest_framework.permissions import AllowAny
-from .serializers import RegisterSerializer
+from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
+from rest_framework.permissions import IsAuthenticated
+from django_filters import rest_framework as filters
+from .models import Movie
+from .permissions import IsOwnerOrReadOnly
+from .serializers import MovieSerializer
+from .pagination import CustomPagination
+from .filters import MovieFilter
 
 
-class RegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    permission_classes = (AllowAny,)
-    serializer_class = RegisterSerializer
+class ListCreateMovieAPIView(ListCreateAPIView):
+    serializer_class = MovieSerializer
+    queryset = Movie.objects.all()
+    permission_classes = [IsAuthenticated]
+    pagination_class = CustomPagination
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = MovieFilter
+
+    def perform_create(self, serializer):
+        # Assign the user who created the movie
+        serializer.save(creator=self.request.user)
+
+
+class RetrieveUpdateDestroyMovieAPIView(RetrieveUpdateDestroyAPIView):
+    serializer_class = MovieSerializer
+    queryset = Movie.objects.all()
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+
+
+
+
