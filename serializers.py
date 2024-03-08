b132203c47fa71daa9de4b8914d18878fc2c00e4
Jasurbek <154021388+JasurbekKarimov@users.noreply.github.com>
2024-03-08 12:40:36 +0500

    Add files via upload

--- serializers.py
+++ serializers.py
@@ -1,19 +1,33 @@
 from rest_framework import serializers
-from .models import Movie
 from django.contrib.auth.models import User
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth.password_validation import validate_password
 
 
-class MovieSerializer(serializers.ModelSerializer):  # create class to serializer model
-    creator = serializers.ReadOnlyField(source='creator.username')
+class RegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
 
-    class Meta:
-        model = Movie
-        fields = ('id', 'title', 'genre', 'year', 'creator')
-
-
-class UserSerializer(serializers.ModelSerializer):  # create class to serializer user model
-    movies = serializers.PrimaryKeyRelatedField(many=True, queryset=Movie.objects.all())
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'movies')
+        fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name')
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+
+        return attrs
+
+    def create(self, validated_data):
+        del validated_data['password2']
+        user = User.objects.create(**validated_data)
+
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
--- urls.py
+++ urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from . import views
-
-
-
-
+from authentication.views import RegisterView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('', views.ListCreateMovieAPIView.as_view(), name='get_post_movies'),
-    path('<int:pk>/', views.RetrieveUpdateDestroyMovieAPIView.as_view(), name='get_delete_update_movie'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', RegisterView.as_view(), name='auth_register'),
 ]
